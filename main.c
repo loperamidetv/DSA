8dabbf8245d405402b1a872317fedff7c486cebc
loperamidetv <bgp97480@gmail.com>
2024-12-11 19:33:45 +0400

    struct modification, no bools anymore and light commenting

--- main.c
+++ main.c
@@ -10,29 +10,26 @@ typedef struct Node {
 
 typedef struct LinkedList {
     Node* head; 
-    unsigned int size; 
-    _Bool is_initialized; 
+    unsigned int size;  // Very convenient because we are not forced to loop through the DS to get the size. Plus, makes the code mode readable.
 } LinkedList; 
 
 // Prototypes
 LinkedList* create_list(); 
-_Bool insert(LinkedList*, int); 
+void insert(LinkedList*, int); 
 void read(LinkedList*); 
 
 int main(int* argc, char** argv) 
 {
     LinkedList* myLinkedList = create_list();  
-    if (myLinkedList->is_initialized) {
-        printf("Linked list successfuly created!\n"); 
-    } else {
-        printf("Failed! "); 
-    }
+    // if malloc fails, exit the program.
+    if (myLinkedList == NULL) return -1; 
 
+    // puts 10 random values into the linked list. Just for test purposes. 
     for(size_t i = 0; i < 10; i++) {
-        insert(myLinkedList, rand()%10); 
+        
+        insert(myLinkedList, rand()%10 + 1); 
     }
 
-    printf("list in size %d\n", myLinkedList->size);
     read(myLinkedList); 
     free(myLinkedList); 
     
@@ -44,25 +41,22 @@ LinkedList* create_list() {
     if (list != NULL){
         list->head = NULL; 
         list->size = 0; 
-        list->is_initialized = true; 
-    }
-    else {
-        list->is_initialized = false; 
     }
     return list; 
 }
 
-_Bool insert(LinkedList* list, int elt) {
+void insert(LinkedList* list, int elt) {
     Node* new_elt = (Node*) malloc(sizeof(Node));
-    if (new_elt == NULL) return false;
+    if (new_elt == NULL) return; // abort execution if malloc failed...
 
+    // Basic initialization of the Node
     new_elt->data = elt; 
     new_elt->next = NULL;  
      
     if (list->size == 0) {
         list->head = new_elt;
         list->size++; 
-        return true; 
+        return; // if successful, exit the function to prevent further execution
     }
    
     Node* ptr_node = list->head; 
@@ -72,17 +66,19 @@ _Bool insert(LinkedList* list, int elt) {
     ptr_node->next = new_elt; 
 
     list->size++; // if not already been ++ed
-    return true; // new element succesfuly added
 }
 
 void read(LinkedList* list) {
     printf("[");
     if (list->size>0) {
-        Node* ptr_node = list->head;
+        // Indeed, if the list is empty, we don't need to go through all that suffering... 
+        Node* ptr_node = list->head; 
         while (ptr_node != NULL) {
+            // Each iteration, we read the next adress and make that address the address of our pointer. 
+            // Of course, iif our pointer points to somewhere... We are not gonna dereference a NULL pointer...
             printf("%d", ptr_node->data); 
             ptr_node = ptr_node->next; 
-            if(ptr_node != NULL) printf(", "); 
+            if(ptr_node != NULL) printf(", "); // ... this line is here for formating purposes... 
         }    
     }
     printf("]"); 
