5dfceead79f8ffee1d7b31d25c2dc955dbb63d30
loperamidetv <bgp97480@gmail.com>
2024-12-11 19:02:10 +0400

    Now insert and read properly

--- main.c
+++ main.c
@@ -16,16 +16,24 @@ typedef struct LinkedList {
 
 // Prototypes
 LinkedList* create_list(); 
+_Bool insert(LinkedList*, int); 
+void read(LinkedList*); 
 
 int main(int* argc, char** argv) 
 {
     LinkedList* myLinkedList = create_list();  
     if (myLinkedList->is_initialized) {
-        printf("Linked list successfuly created!"); 
+        printf("Linked list successfuly created!\n"); 
     } else {
         printf("Failed! "); 
     }
-    
+
+    for(size_t i = 0; i < 10; i++) {
+        insert(myLinkedList, rand()%10); 
+    }
+
+    printf("list in size %d\n", myLinkedList->size);
+    read(myLinkedList); 
     free(myLinkedList); 
     
     return 0; 
@@ -42,4 +50,40 @@ LinkedList* create_list() {
         list->is_initialized = false; 
     }
     return list; 
+}
+
+_Bool insert(LinkedList* list, int elt) {
+    Node* new_elt = (Node*) malloc(sizeof(Node));
+    if (new_elt == NULL) return false;
+
+    new_elt->data = elt; 
+    new_elt->next = NULL;  
+     
+    if (list->size == 0) {
+        list->head = new_elt;
+        list->size++; 
+        return true; 
+    }
+   
+    Node* ptr_node = list->head; 
+    while(ptr_node->next != NULL) {
+        ptr_node = ptr_node->next; 
+    }
+    ptr_node->next = new_elt; 
+
+    list->size++; // if not already been ++ed
+    return true; // new element succesfuly added
+}
+
+void read(LinkedList* list) {
+    printf("[");
+    if (list->size>0) {
+        Node* ptr_node = list->head;
+        while (ptr_node != NULL) {
+            printf("%d", ptr_node->data); 
+            ptr_node = ptr_node->next; 
+            if(ptr_node != NULL) printf(", "); 
+        }    
+    }
+    printf("]"); 
 }
